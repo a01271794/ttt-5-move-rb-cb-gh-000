02dc89d181d65f6b9caf730a77e3ada930fb6ad9
Oscar Frederick Perez Reyes <a01271794@itesm.mx>
2019-02-06 03:06:23 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -13,11 +13,7 @@ def input_to_index(user_input)
   converted_input=user_input.to_i-1
 end
 
-def update_array_at_with(array, index, value)
-  array[index] = value
-end
 
-update_array_at_with(board, 0, "X")
 
 def move(array, index=0, value="X")
   array[index] = value
