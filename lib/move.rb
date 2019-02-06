351df2b62359c5c2e9e05421a8d8a29e168c1395
Oscar Frederick Perez Reyes <a01271794@itesm.mx>
2019-02-06 03:04:31 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -10,7 +10,7 @@ end
 
 # code your input_to_index and move method here!
 def input_to_index(user_input)
-  converted_input=index-1
+  converted_input=user_input-1
 end
 
 def update_array_at_with(array, index, value)
